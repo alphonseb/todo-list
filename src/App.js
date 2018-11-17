098ba3cca2e36bf8d65687e521056e468075f572
alphonseb <alphonse.bouy@gmail.com>
2018-11-17 16:10:46 +0100

    :sparkles: netlify identity

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from 'react'
 import './static/css/app.css'
 import { Todolist } from './components/Todolist'
+import netlifyIdentity from 'netlify-identity-widget'
 
 class App extends Component {
+    state = {
+        isAuthenticated: false
+    }
+
+    login = () => {
+        netlifyIdentity.open()
+        netlifyIdentity.on('login', () => {
+            console.log('logged in')
+        })
+    }
+
     render() {
         return (
             <div className="App">
-                <Todolist />
+                <button onClick={this.login}>Log in</button>
+                {/* <Todolist /> */}
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import * as serviceWorker from './serviceWorker'
+import netlifyIdentity from 'netlify-identity-widget'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+window.netlifyIdentity = netlifyIdentity
+// You must run this once before trying to interact with the widget
+netlifyIdentity.init()
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
